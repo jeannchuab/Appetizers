0ffae5eacf13eded24329fcc17554b8d0c521543
Jeann Chuab <jeann.chuab@gmail.com>
2024-02-23 11:43:34 -0300

    Adding account fields.

--- Appetizers/Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Appetizers/Screens/AccountView/AccountView.swift
@@ -8,10 +8,58 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var textFieldFirstName = ""
+    @State private var textFieldLastName = ""
+    @State private var textFieldEmail = ""
+    @State private var textFieldBirthday = Date()
+    @State private var toggleExtraNapkings = false
+    @State private var toggleFrequentRefills = false
+    
     var body: some View {
         NavigationView {
-            Text("Account view")
-                .navigationTitle("👨‍💼 Account")
+            
+            Form {
+                
+                Section(content: {
+                    TextField("First name", text: $textFieldFirstName)
+                        .autocorrectionDisabled(false)
+                        
+                    TextField("Last name", text: $textFieldLastName)
+                        .autocorrectionDisabled(false)
+                    
+                    TextField("Email", text: $textFieldEmail)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled(false)
+                                                            
+                    DatePicker("Birthday", selection: $textFieldBirthday,
+                               displayedComponents: .date)
+                                        
+                    Button(action: {
+                        print("Click buton")
+                    }, label: {
+                        Text("Save changes")
+                    })
+                    .tint(Color.brandPrimary)
+                    
+                }, header: { 
+                    Text("Personal Info")
+                })
+                
+                Section(content: {
+                    Toggle("Extra Napkings", isOn: $toggleExtraNapkings)
+//                         .tint(Color.brandPrimary)
+                        
+                    Toggle("Frequent Refills", isOn: $toggleFrequentRefills)
+//                        .tint(Color.brandPrimary)
+                    
+                }, header: {
+                    Text("Requests")
+                })
+                .tint(Color.brandPrimary)
+            }
+            .navigationTitle("👨‍💼 Account")
         }
     }
 }
